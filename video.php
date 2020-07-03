fce9071aae41d4ceaa0d2d110f7782f0c87423ce
xiaoboke <q409640972@126.com>
2020-07-03 09:22:05 +0800

    init

---
+++ video.php
@@ -0,0 +1,18 @@
+<?php
+
+function read(...$filelist) {
+    $list = [];
+    foreach ($filelist as $file) {
+        $handle = fopen($file, 'r');
+        while (($line = fgets($handle)) !== false) {
+            array_push($list, trim($line));
+        }
+        fclose($handle);
+    }
+    return $list;
+}
+
+$list = read('ks.txt');
+$url = $list[array_rand($list)];
+
+header("Location: {$url}");
